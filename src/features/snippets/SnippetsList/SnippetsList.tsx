22ba06298efc2637435bb54a72a2360950f90445
Diana Shilova <Shilova_de@mail.ru>
2024-02-04 19:26:17 +0300

    edit css step 1

--- src/features/snippets/SnippetsList/SnippetsList.tsx
+++ src/features/snippets/SnippetsList/SnippetsList.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Snippet } from '../../../types';
 
 type Props = {
@@ -19,20 +20,53 @@ export const SnippetsList = (props: Props) => {
     };
 
   return (
-    <ul>
+    <Root>
       {snippets?.map((snippet) => (
-        <li key={snippet.id}>
+        <Line key={snippet.id}>
           <div>
-            <input
+            {/* <input
               type="checkbox"
               onChange={handleChange(snippet)}
               defaultChecked={snippet.checked}
-            />
+            /> */}
             {snippet.title}
           </div>
-          <div>{snippet.prefix}</div>
-        </li>
+          <div>
+            <SnippetPrefix>{snippet.prefix}</SnippetPrefix>
+            {/* <Component /> */}
+          </div>
+        </Line>
       ))}
-    </ul>
+    </Root>
   );
 };
+
+const Root = styled.ul`
+  background: #212334;
+  list-style-type: none;
+  width: 240px;
+  height: 610px;
+  border-radius: 4px;
+  padding-left: 0;
+  padding-top: 55px;
+  margin-top: 45px;
+`;
+
+const Line = styled.li`
+  font: inter;
+  display: flex;
+  flex-direction: column;
+  height: 38px;
+  width: 224px;
+  background: #1f2130;
+  font-size: 12px;
+  justify-content: start;
+  margin: 8px 18px;
+  align-items: flex-start;
+`;
+
+const SnippetPrefix = styled.em`
+  height: 13px;
+  width: 125;
+  color: #e6e6e699;
+`;
